6ae72dcaa7c7f62cbdf3e51d7d5b90280766f0dc
PrinceKomali <taborinstein@gmail.com>
2022-11-02 15:54:51 -0500

    Main game done [11/2/22]

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 let island_img = document.querySelector(".island-img");
+let date_str = _ => (new Date).toString().split(/\d\d:\d\d:\d\d/)[0];
 function drandom() {
     //stackoverflow.com/q/521295
-    let str = window.btoa((new Date).toString().split(/\d\d:\d\d:\d\d/)[0]);
+    let str = window.btoa(date_str());
     let h1 = 1779033703, h2 = 3144134277,
         h3 = 1013904242, h4 = 2773480762;
     for (let i = 0, k; i < str.length; i++) {
@@ -29,19 +30,52 @@ class Islandle {
         this.INCORRECT = 2;
         this.CORRECT = 3;
 
+        this.done = false;
         this.guesses = [];
         this.index = drandom() % ISLANDS.length;
         this.island = ISLANDS[this.index];
     }
     init () {
         let Island = ISLANDS[drandom() % ISLANDS.length];
-        // for (let i of ISLANDS) {
-        //     o = document.createElement("option");
-        //     o.value = i.name;
-        //     document.querySelector(".temp-datalist").appendChild(o)
-        // }
+        for(let i = 0; i < 10; i++) {
+            let item = localStorage.getItem(date_str().replace(/ /g,'') + `-G${i}`);
+            if(item) {
+                this.guess(item);
+                if(item == fmt(this.island.name)) this.finish(this.CORRECT);
+                if(i == 9) this.finish(this.INCORRECT);
+            }
+            else break;
+        }
 
         island_img.src = `images/${Island.image}`;
+        island_img.addEventListener("contextmenu", _ => _.preventDefault());
+    }
+    finish(ret) {
+        this.done = true;
+        guess_btn.style.display = "none";
+        share_btn.style.display = "block";
+        guess_input.readOnly = true;
+        guess_input.placeholder = ret == Game.CORRECT ? "You got it!" : "Maybe next time!";
+        let {island} = Game;
+        setTimeout(_=>{
+            let i = 0;
+            autocomplete.innerHTML = '';
+            let message = [
+                `Answer: <b>${island.territory ? `<img class="flag" height="1em" src="https://flagcdn.com/16x12/${island.territory}.webp" />&nbsp;` : ""}${island.name}</b>`,
+                `${island.claims.type} ${type_grammar(island.claims.type)} ${
+                    island.claims.countries.map(c=>`<img class="flag" height="1em" src="https://flagcdn.com/16x12/${c}.webp" />` + countries.find(x=>c == x.code.toLowerCase()).name).join(" and ")
+                }`,
+                `Population: <b>${island.population < 1 ? "Uninhabited" : island.population}</b>`,
+                `<a class="map_link" href="https://google.com/maps/search/?api=1&query=${island.lat}%2C${island.long}" target="_blank">Link</a>`
+            
+            ];
+
+            let _int = setInterval(_=> {
+                if(message.length < 1) return clearInterval(_int);
+                autocomplete.innerHTML += `${i < 1 ? "" : "<br>"}${message.shift()}`
+                i++;
+            }, 10);
+        },100);
     }
     generateSquares(num_squares, guess_distance) {
         let percentage = Math.floor((Math.max(2_000_000-guess_distance*100,0)/2_000_000)*100);
@@ -55,8 +89,12 @@ class Islandle {
         return {squares, percentage}
     }
     pushGuess(guessed) {
-        //google.com/maps/search/?api=1&query=-37.1052%2C-12.2777
         this.guesses.push(guessed);
+        localStorage.setItem(
+            date_str().replace(/ /g,'') +
+            `-G${this.guesses.length - 1}`,
+            fmt(guessed.name)
+        )
         let guess_elem = document.querySelectorAll(".guess")[this.guesses.length - 1];
 
 
